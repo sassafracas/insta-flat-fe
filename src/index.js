491f58c2f615321e675672de4ca6f55c2188f3c1
github username <torpid@gmail.com>
2018-06-14 15:22:15 -0400

    nearly there

--- src/index.js
+++ src/index.js
@@ -5,3 +5,76 @@
 //        <span id="likes-count-for-image-1">41</span>
 //    </p>
 //</div>
+
+document.addEventListener("DOMContentLoaded", function (){
+  const imagesURL = "http://localhost:3000/api/v1/images"
+  const likesURL = "http://localhost:3000/api/v1/likes"
+  const likeButton = document.getElementById("like-button")
+  const inputForm = document.getElementById("post-image-form")
+  const inputFormInput = document.getElementById("post-image-form-url")
+
+  inputForm.addEventListener("submit", function (event){
+    event.preventDefault()
+    const config = {
+      method: "POST",
+      headers: {
+        "Content-Type" : "application/json"
+      },
+      body: {
+        url : JSON.stringify({url:inputFormInput.value})
+      }
+    }
+    console.log(inputFormInput.value)
+    fetch(imagesURL, config).then(response => response.json()).then(response => console.log(response))
+  })
+
+  function getAllImages () {
+    fetch(imagesURL).then(response => response.json()).then(getOneImage)
+  }
+
+  function getOneImage(imageObjArray) {
+    imageObjArray.forEach(generateImageHTML)
+  }
+
+  function generateImageHTML(imageObj) {
+    const div = document.createElement('div')
+    const img = document.createElement('img')
+    const p = document.createElement("p")
+    const outerDiv = document.getElementById("container")
+
+
+
+    const like = `
+    <img data-action="like-image" data-image-id=${imageObj.id} class="like-button" id="like-button" src="./images/like.png"><br>
+    //        <span id="likes-count-for-image-1">${imageObj.likes_count}</span>
+    `
+
+    div.setAttribute("class", "image-container")
+    img.setAttribute("src", `${imageObj.url}`)
+    outerDiv.appendChild(div)
+    div.appendChild(img)
+    div.appendChild(p)
+    p.innerHTML = like
+    div.addEventListener("click", function (event){
+      if (event.target.id === "like-button"){
+        const config = {
+          method: "POST",
+          headers: {
+            "Content-type" : "application/json"
+          },
+          body: JSON.stringify({image_id:event.target.dataset.imageId})
+        }
+        debugger
+        fetch(likesURL, config).then(response => response.json()).then(response => console.log(response))
+      }
+    })
+  }
+
+
+  function startApp () {
+    getAllImages()
+  }
+
+
+  startApp()
+})
